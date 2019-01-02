418ae95f96e0aa1e9ca1229bc948abf28223deb0
Dark Young <16302010059@fudan.edu.cn>
2019-01-02 19:36:14 +0800

    update DDL & DML

--- sql/pms_DDL.sql
+++ sql/pms_DDL.sql
@@ -25,133 +25,133 @@ DROP TABLE IF EXISTS community;
 
 CREATE TABLE community (
 	community_id		varchar(8) NOT NULL,
-	community_name		varchar(64) NOT NULL,
-	pks_purchase_fee	numeric(10, 2) CHECK (pks_purchase_fee >= 0),
-	pks_rental_fee		numeric(8, 2) CHECK (pks_rental_fee >= 0),
-	residence_price		numeric(8, 2) CHECK (residence_price >= 0),
+	community_name		varchar(255) NOT NULL,
+	pks_purchase_fee	numeric(20, 2) CHECK (pks_purchase_fee >= 0),
+	pks_rental_fee		numeric(15, 2) CHECK (pks_rental_fee >= 0),
+	residence_price		numeric(10, 2) CHECK (residence_price >= 0),
 	PRIMARY KEY (community_id)
-); 
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic; 
 
 CREATE TABLE residence (
 	residence_id	varchar(8) NOT NULL,
-	floor_num	numeric(3, 0) NOT NULL,
-	unit_num	numeric(4, 0) CHECK (unit_num > 0),
-	room_num	numeric(5, 0) CHECK (room_num > 0),
-	area		numeric(6, 2) CHECK (area > 0),
+	floor_num	numeric(5, 0) NOT NULL,
+	unit_num	numeric(6, 0) CHECK (unit_num > 0),
+	room_num	numeric(8, 0) CHECK (room_num > 0),
+	area		numeric(10, 2) CHECK (area > 0),
 	PRIMARY KEY (residence_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE residence_record (
 	community_id	varchar(8) NOT NULL,
 	residence_id	varchar(8) NOT NULL,
-	residence_state	varchar(3) CHECK (residence_state IN ('YES', 'NO')),
+	residence_state	varchar(5) CHECK (residence_state IN ('YES', 'NO')),
 	PRIMARY KEY (community_id, residence_id),
 	FOREIGN KEY (community_id) REFERENCES community(community_id) ON DELETE CASCADE,
 	FOREIGN KEY (residence_id) REFERENCES residence(residence_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE resident (
 	resident_id	varchar(8) NOT NULL,
-	resident_name	varchar(20) NOT NULL,
-	phonenumber	varchar(22),
+	resident_name	varchar(255) NOT NULL,
+	phonenumber	varchar(255),
 	PRIMARY KEY (resident_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE property_record (
 	resident_id	varchar(8) NOT NULL,
 	residence_id	varchar(8) NOT NULL,
 	issue_date	timestamp NOT NULL,
-	purchased_fee	numeric(8, 2) CHECK (purchased_fee >= 0),
+	purchased_fee	numeric(20, 2) CHECK (purchased_fee >= 0),
 	PRIMARY KEY (resident_id, residence_id),
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id),
 	FOREIGN KEY (residence_id) REFERENCES residence(residence_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE parking_space (
 	pks_id		varchar(6) NOT NULL,
 	community_id	varchar(8) NOT NULL,
-	pks_state	varchar(5) CHECK (pks_state IN ('IDLE', 'BUSY')),
-	pks_type	varchar(5) CHECK (pks_type IN ('TEMPORARY', 'RENTED', 'PURCHASED')),
+	pks_state	varchar(10) DEFAULT 'IDLE' CHECK (pks_state IN ('IDLE', 'BUSY')),
+	pks_type	varchar(15) CHECK (pks_type IN ('TEMPORARY', 'RENTED', 'PURCHASED')),
 	PRIMARY KEY (pks_id, community_id),
 	FOREIGN KEY (community_id) REFERENCES community(community_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE temporary_pks (
 	pks_id		varchar(6) NOT NULL,
 	PRIMARY KEY (pks_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE rented_pks (
 	pks_id		varchar(6) NOT NULL,
 	resident_id	varchar(8) NOT NULL,
-	man_fee		numeric(5, 2) CHECK (man_fee >= 0),
+	man_fee		numeric(8, 2) DEFAULT 0 CHECK (man_fee >= 0),
 	PRIMARY KEY (pks_id),
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE purchased_pks (
 	pks_id		varchar(6) NOT NULL,
 	resident_id	varchar(8) NOT NULL,
-	man_fee		numeric(5, 2) CHECK (man_fee >= 0),
+	man_fee		numeric(8, 2) DEFAULT 0 CHECK (man_fee >= 0),
 	PRIMARY KEY (pks_id),
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE vehicle (
 	license_plate	varchar(20) NOT NULL,
 	owner_name	varchar(15) NOT NULL,
 	PRIMARY KEY (license_plate)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE payment_record (
-	payment_id	varchar(6) NOT NULL,
-	payment		numeric(5, 2) CHECK (payment >= 0),
-	duration	numeric(3, 1) CHECK (duration > 0),
+	payment_id	varchar(32) NOT NULL,
+	payment		numeric(20, 2) CHECK (payment >= 0),
+	duration	numeric(5, 1) CHECK (duration > 0),
 	start_time	timestamp NOT NULL,
-	checked		varchar(3) CHECK (checked in ('YES', 'NO')),
+	checked		varchar(5) CHECK (checked in ('YES', 'NO')),
 	PRIMARY KEY (payment_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE property_fee_record (
-	payment_id	varchar(6) NOT NULL,
+	payment_id	varchar(32) NOT NULL,
 	resident_id	varchar(8) NOT NULL,
 	PRIMARY KEY (payment_id, resident_id),
 	FOREIGN KEY (payment_id) REFERENCES payment_record(payment_id) ON DELETE CASCADE,
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE pks_fee_record (
-	payment_id	varchar(6) NOT NULL,
+	payment_id	varchar(32) NOT NULL,
 	pks_id		varchar(6) NOT NULL,
 	description	varchar(1024) NOT NULL,
 	PRIMARY KEY (payment_id, pks_id),
 	FOREIGN KEY (payment_id) REFERENCES payment_record(payment_id) ON DELETE CASCADE,
 	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE pks_management_fee_record (
-	payment_id	varchar(6) NOT NULL,
+	payment_id	varchar(32) NOT NULL,
 	pks_id		varchar(6) NOT NULL,
 	PRIMARY KEY (payment_id, pks_id),
 	FOREIGN KEY (payment_id) REFERENCES payment_record(payment_id) ON DELETE CASCADE,
 	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 	
 CREATE TABLE equipment_info (
-	category	varchar(6) CHECK (category IN ('OUTDOOR', 'INDOOR')),
-	equipment_type	varchar(15) NOT NULL,
-	check_period	numeric(2, 0) CHECK (check_period > 0),
-	repair_fee	numeric(5, 2) CHECK (repair_fee >= 0),
+	category	varchar(10) CHECK (category IN ('OUTDOOR', 'INDOOR')),
+	equipment_type	varchar(64) NOT NULL,
+	check_period	numeric(3, 0) DEFAULT 1 CHECK (check_period > 0) ,
+	repair_fee	numeric(5, 2) DEFAULT 0 CHECK (repair_fee >= 0),
 	PRIMARY KEY (category, equipment_type)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE equipment (
 	equipment_id	varchar(6) NOT NULL,
-	category	varchar(6) NOT NULL,
-	equipment_type	varchar(15) NOT NULL,
+	category	varchar(10) NOT NULL,
+	equipment_type	varchar(64) NOT NULL,
 	PRIMARY KEY (equipment_id),
 	FOREIGN KEY (category, equipment_type) REFERENCES equipment_info(category, equipment_type) ON DELETE CASCADE
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE equipment_record (
 	community_id	varchar(8) NOT NULL,
@@ -159,7 +159,7 @@ CREATE TABLE equipment_record (
 	PRIMARY KEY (community_id, equipment_id),
 	FOREIGN KEY (community_id) REFERENCES community(community_id),
 	FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 
 CREATE TABLE repair_order (
@@ -168,39 +168,39 @@ CREATE TABLE repair_order (
 	equipment_id	varchar(6) NOT NULL,
 	issue_date	timestamp NOT NULL,
 	repair_reason	varchar(1024),
-	processing	varchar(12) CHECK (processing in ('TOBE', 'PROCESSING', 'DONE')),
+	processing	varchar(12) DEFAULT 'TODO' CHECK (processing in ('TODO', 'PROCESSING', 'DONE')),
 	process_result	varchar(1024),
 	PRIMARY KEY (repair_id, resident_id, equipment_id),
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id),
 	FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE inspect_record (
 	equipment_id		varchar(6) NOT NULL,
 	issue_date		timestamp NOT NULL,
-	inspector		varchar(20) NOT NULL,
-	waiting_for_repair	varchar(3) CHECK (waiting_for_repair IN ('YES', 'NO')),
+	inspector		varchar(255) NOT NULL,
+	waiting_for_repair	varchar(5) CHECK (waiting_for_repair IN ('YES', 'NO')),
 	PRIMARY KEY (equipment_id, issue_date),
 	FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE maintenance_record (
 	equipment_id		varchar(6) NOT NULL,
 	issue_date		timestamp NOT NULL,
-	maintenance_worker	varchar(20) NOT NULL,
+	maintenance_worker	varchar(255) NOT NULL,
 	maintenance_result	varchar(1024),
 	PRIMARY KEY (equipment_id, issue_date),
 		FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE complaint_order (
 	complaint_id	varchar(6) NOT NULL,
 	residence_id	varchar(8) NOT NULL,
-	category	varchar(22),
+	category	varchar(64),
 	issue_date	timestamp NOT NULL,
 	complaint_result varchar(1024),
 	PRIMARY KEY (complaint_id, residence_id),
 	FOREIGN KEY (residence_id) REFERENCES residence(residence_id)
-);
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
---
+++ sql/pms_DML.sql
@@ -0,0 +1,16 @@
+INSERT INTO community VALUES('c1', '小区A', 200000, 200, 1);
+INSERT INTO community VALUES('c2', '小区B', 220000, 250, 1.5);
+INSERT INTO community VALUES('c3', '小区C', 240000, 300, 2);
+
+INSERT INTO equipment_info VALUES('OUTDOOR', '健身设备', 6, 100);
+INSERT INTO equipment_info VALUES('OUTDOOR', '小区内照明灯', 6, 10);
+INSERT INTO equipment_info VALUES('OUTDOOR', '公告牌', 6, 15);
+INSERT INTO equipment_info VALUES('OUTDOOR', '躺椅', 6, 40);
+INSERT INTO equipment_info VALUES('INDOOR', '楼道声控灯', 1, 30);
+INSERT INTO equipment_info VALUES('INDOOR', '楼房门禁设备', 1, 120);
+INSERT INTO equipment_info VALUES('INDOOR', '电梯', 1, 300);
+INSERT INTO equipment_info VALUES('INDOOR', '楼层照明灯', 1, 10);
+INSERT INTO equipment_info VALUES('INDOOR', '水闸', 1, 10);
+INSERT INTO equipment_info VALUES('INDOOR', '电闸', 1, 20);
+
+
