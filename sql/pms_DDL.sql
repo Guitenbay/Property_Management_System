df437c044896f056e79f6c6d3aa3a2e3e5cbfee8
Dark Young <16302010059@fudan.edu.cn>
2019-01-03 21:06:34 +0800

    change the data type of id to int

--- sql/pms_DDL.sql
+++ sql/pms_DDL.sql
@@ -12,11 +12,11 @@ DROP TABLE IF EXISTS pks_management_fee_record;
 DROP TABLE IF EXISTS pks_fee_record;
 DROP TABLE IF EXISTS property_fee_record;
 DROP TABLE IF EXISTS payment_record;
-DROP TABLE IF EXISTS vehicle;
 DROP TABLE IF EXISTS purchased_pks;
 DROP TABLE IF EXISTS rented_pks;
 DROP TABLE IF EXISTS temporary_pks;
 DROP TABLE IF EXISTS parking_space;
+DROP TABLE IF EXISTS vehicle;
 DROP TABLE IF EXISTS property_record;
 DROP TABLE IF EXISTS resident;
 DROP TABLE IF EXISTS residence_record;
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS residence;
 DROP TABLE IF EXISTS community;
 
 CREATE TABLE community (
-	community_id		varchar(8) NOT NULL,
+	community_id      INT NOT NULL AUTO_INCREMENT,
 	community_name		varchar(255) NOT NULL,
 	pks_purchase_fee	numeric(20, 2) CHECK (pks_purchase_fee >= 0),
 	pks_rental_fee		numeric(15, 2) CHECK (pks_rental_fee >= 0),
@@ -33,7 +33,7 @@ CREATE TABLE community (
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic; 
 
 CREATE TABLE residence (
-	residence_id	varchar(8) NOT NULL,
+	residence_id	INT NOT NULL AUTO_INCREMENT,
 	floor_num	numeric(5, 0) NOT NULL,
 	unit_num	numeric(6, 0) CHECK (unit_num > 0),
 	room_num	numeric(8, 0) CHECK (room_num > 0),
@@ -42,8 +42,8 @@ CREATE TABLE residence (
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE residence_record (
-	community_id	varchar(8) NOT NULL,
-	residence_id	varchar(8) NOT NULL,
+	community_id	INT NOT NULL,
+	residence_id	INT NOT NULL,
 	residence_state	varchar(5) CHECK (residence_state IN ('IDLE', 'BUSY')),
 	PRIMARY KEY (community_id, residence_id),
 	FOREIGN KEY (community_id) REFERENCES community(community_id) ON DELETE CASCADE,
@@ -51,55 +51,20 @@ CREATE TABLE residence_record (
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE resident (
-	resident_id	varchar(8) NOT NULL,
+	resident_id	  INT NOT NULL AUTO_INCREMENT,
 	resident_name	varchar(255) NOT NULL,
 	phonenumber	varchar(255),
 	PRIMARY KEY (resident_id)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE property_record (
-	resident_id	varchar(8) NOT NULL,
-	residence_id	varchar(8) NOT NULL,
+	resident_id	  INT NOT NULL,
+	residence_id	INT NOT NULL,
 	issue_date	timestamp NOT NULL,
 	purchased_fee	numeric(20, 2) CHECK (purchased_fee >= 0),
 	PRIMARY KEY (resident_id, residence_id),
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id),
 	FOREIGN KEY (residence_id) REFERENCES residence(residence_id)
-)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-CREATE TABLE parking_space (
-	pks_id		varchar(6) NOT NULL,
-	community_id	varchar(8) NOT NULL,
-	pks_state	varchar(10) DEFAULT 'IDLE' CHECK (pks_state IN ('IDLE', 'BUSY')),
-	pks_type	varchar(15) CHECK (pks_type IN ('TEMPORARY', 'RENTED', 'PURCHASED')),
-	PRIMARY KEY (pks_id),
-	FOREIGN KEY (community_id) REFERENCES community(community_id)
-)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-CREATE TABLE temporary_pks (
-	pks_id		varchar(6) NOT NULL,
-	discount  numeric(2, 2) CHECK (discount >= 0 AND discount <= 1),
-	discount_price numeric(20, 2) CHECK (discount_price >= 0),
-	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id) ON DELETE CASCADE,
-	PRIMARY KEY (pks_id)
-)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-CREATE TABLE rented_pks (
-	pks_id		varchar(6) NOT NULL,
-	resident_id	varchar(8) NOT NULL,
-	man_fee		numeric(8, 2) DEFAULT 0 CHECK (man_fee >= 0),
-	PRIMARY KEY (pks_id),
-	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id) ON DELETE CASCADE,
-	FOREIGN KEY (resident_id) REFERENCES resident(resident_id)
-)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-CREATE TABLE purchased_pks (
-	pks_id		varchar(6) NOT NULL,
-	resident_id	varchar(8) NOT NULL,
-	man_fee		numeric(8, 2) DEFAULT 0 CHECK (man_fee >= 0),
-	PRIMARY KEY (pks_id),
-	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id) ON DELETE CASCADE,
-	FOREIGN KEY (resident_id) REFERENCES resident(resident_id)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE vehicle (
@@ -108,8 +73,45 @@ CREATE TABLE vehicle (
 	PRIMARY KEY (license_plate)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
+CREATE TABLE parking_space (
+	pks_id		INT NOT NULL AUTO_INCREMENT,
+	community_id  INT NOT NULL,
+	pks_state	varchar(10) DEFAULT 'IDLE' CHECK (pks_state IN ('IDLE', 'BUSY')),
+	pks_type	varchar(15) CHECK (pks_type IN ('TEMPORARY', 'RENTED', 'PURCHASED')),
+	PRIMARY KEY (pks_id),
+	FOREIGN KEY (community_id) REFERENCES community(community_id)
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+CREATE TABLE temporary_pks (
+	pks_id		INT NOT NULL,
+	license_plate varchar(20),
+	PRIMARY KEY (pks_id),
+	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id) ON DELETE CASCADE,
+	FOREIGN KEY (license_plate) REFERENCES vehicle(license_plate) ON DELETE SET NULL
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+CREATE TABLE rented_pks (
+	pks_id		INT NOT NULL,
+	resident_id	INT,
+	discount  numeric(2, 2) CHECK (discount >= 0 AND discount <= 1),
+	discount_price numeric(20, 2) CHECK (discount_price >= 0),
+	man_fee		numeric(8, 2) DEFAULT 0 CHECK (man_fee >= 0),
+	PRIMARY KEY (pks_id),
+	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id) ON DELETE CASCADE,
+	FOREIGN KEY (resident_id) REFERENCES resident(resident_id) ON DELETE SET NULL
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+CREATE TABLE purchased_pks (
+	pks_id		  INT NOT NULL,
+	resident_id	INT,
+	man_fee		numeric(8, 2) DEFAULT 0 CHECK (man_fee >= 0),
+	PRIMARY KEY (pks_id),
+	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id) ON DELETE CASCADE,
+	FOREIGN KEY (resident_id) REFERENCES resident(resident_id) ON DELETE SET NULL
+)ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
 CREATE TABLE payment_record (
-	payment_id	varchar(32) NOT NULL,
+	payment_id	INT NOT NULL AUTO_INCREMENT,
 	payment		numeric(20, 2) CHECK (payment >= 0),
 	duration	numeric(5, 1) CHECK (duration > 0),
 	start_time	timestamp NOT NULL,
@@ -118,16 +120,16 @@ CREATE TABLE payment_record (
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE property_fee_record (
-	payment_id	varchar(32) NOT NULL,
-	resident_id	varchar(8) NOT NULL,
+	payment_id	INT NOT NULL,
+	resident_id	INT NOT NULL,
 	PRIMARY KEY (payment_id, resident_id),
 	FOREIGN KEY (payment_id) REFERENCES payment_record(payment_id) ON DELETE CASCADE,
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE pks_fee_record (
-	payment_id	varchar(32) NOT NULL,
-	pks_id		varchar(6) NOT NULL,
+	payment_id	INT NOT NULL,
+	pks_id		INT NOT NULL,
 	description	varchar(1024) NOT NULL,
 	PRIMARY KEY (payment_id, pks_id),
 	FOREIGN KEY (payment_id) REFERENCES payment_record(payment_id) ON DELETE CASCADE,
@@ -135,32 +137,32 @@ CREATE TABLE pks_fee_record (
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE pks_management_fee_record (
-	payment_id	varchar(32) NOT NULL,
-	pks_id		varchar(6) NOT NULL,
+	payment_id	INT NOT NULL,
+	pks_id		INT NOT NULL,
 	PRIMARY KEY (payment_id, pks_id),
 	FOREIGN KEY (payment_id) REFERENCES payment_record(payment_id) ON DELETE CASCADE,
 	FOREIGN KEY (pks_id) REFERENCES parking_space(pks_id)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 	
 CREATE TABLE equipment_info (
-	category	varchar(10) CHECK (category IN ('OUTDOOR', 'INDOOR')),
+	category	      varchar(10) DEFAULT 'INDOOR' CHECK (category IN ('OUTDOOR', 'INDOOR')),
 	equipment_type	varchar(64) NOT NULL,
-	check_period	numeric(3, 0) DEFAULT 1 CHECK (check_period > 0) ,
-	repair_fee	numeric(5, 2) DEFAULT 0 CHECK (repair_fee >= 0),
+	check_period	  numeric(3, 0) DEFAULT 1 CHECK (check_period > 0) ,
+	repair_fee	    numeric(5, 2) DEFAULT 0 CHECK (repair_fee >= 0),
 	PRIMARY KEY (category, equipment_type)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE equipment (
-	equipment_id	varchar(6) NOT NULL,
-	category	varchar(10) NOT NULL,
+	equipment_id	  INT NOT NULL AUTO_INCREMENT,
+	category	      varchar(10) NOT NULL,
 	equipment_type	varchar(64) NOT NULL,
 	PRIMARY KEY (equipment_id),
 	FOREIGN KEY (category, equipment_type) REFERENCES equipment_info(category, equipment_type) ON DELETE CASCADE
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE equipment_record (
-	community_id	varchar(8) NOT NULL,
-	equipment_id	varchar(6) NOT NULL,
+	community_id	INT NOT NULL,
+	equipment_id	INT NOT NULL,
 	PRIMARY KEY (community_id, equipment_id),
 	FOREIGN KEY (community_id) REFERENCES community(community_id),
 	FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
@@ -168,30 +170,30 @@ CREATE TABLE equipment_record (
 
 
 CREATE TABLE repair_order (
-	repair_id	varchar(6) NOT NULL,
-	resident_id	varchar(8) NOT NULL,
-	equipment_id	varchar(6) NOT NULL,
+	repair_id	  INT NOT NULL AUTO_INCREMENT,
+	resident_id	INT NOT NULL,
+	equipment_id	INT NOT NULL,
 	issue_date	timestamp NOT NULL,
 	repair_reason	varchar(1024),
 	processing	varchar(12) DEFAULT 'TODO' CHECK (processing in ('TODO', 'PROCESSING', 'DONE')),
 	process_result	varchar(1024),
-	PRIMARY KEY (repair_id, resident_id, equipment_id),
+	PRIMARY KEY (repair_id),
 	FOREIGN KEY (resident_id) REFERENCES resident(resident_id),
 	FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE inspect_record (
-	equipment_id		varchar(6) NOT NULL,
-	issue_date		timestamp NOT NULL,
-	inspector		varchar(255) NOT NULL,
-	waiting_for_repair	varchar(5) CHECK (waiting_for_repair IN ('YES', 'NO')),
+	equipment_id		INT NOT NULL,
+	issue_date		  timestamp NOT NULL,
+	inspector		    varchar(255) NOT NULL,
+	waiting_for_repair	varchar(5) DEFAULT 'NO' CHECK (waiting_for_repair IN ('YES', 'NO')),
 	PRIMARY KEY (equipment_id, issue_date),
 	FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE maintenance_record (
-	equipment_id		varchar(6) NOT NULL,
-	issue_date		timestamp NOT NULL,
+	equipment_id		INT NOT NULL,
+	issue_date		  timestamp NOT NULL,
 	maintenance_worker	varchar(255) NOT NULL,
 	maintenance_result	varchar(1024),
 	PRIMARY KEY (equipment_id, issue_date),
@@ -199,12 +201,12 @@ CREATE TABLE maintenance_record (
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 CREATE TABLE complaint_order (
-	complaint_id	varchar(6) NOT NULL,
-	residence_id	varchar(8) NOT NULL,
+	complaint_id	INT NOT NULL AUTO_INCREMENT,
+	residence_id	INT NOT NULL,
 	category	varchar(64),
 	issue_date	timestamp NOT NULL,
 	complaint_result varchar(1024),
-	PRIMARY KEY (complaint_id, residence_id),
+	PRIMARY KEY (complaint_id),
 	FOREIGN KEY (residence_id) REFERENCES residence(residence_id)
 )ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- sql/pms_DML.sql
+++ sql/pms_DML.sql
@@ -1,6 +1,6 @@
-INSERT INTO community VALUES('c1', '小区A', 200000, 200, 1);
-INSERT INTO community VALUES('c2', '小区B', 220000, 250, 1.5);
-INSERT INTO community VALUES('c3', '小区C', 240000, 300, 2);
+INSERT INTO community(community_name, pks_purchase_fee,pks_rental_fee,residence_price) VALUES('A', 200000, 200, 1);
+INSERT INTO community(community_name, pks_purchase_fee,pks_rental_fee,residence_price) VALUES('B', 220000, 250, 1.5);
+INSERT INTO community(community_name, pks_purchase_fee,pks_rental_fee,residence_price) VALUES('C', 240000, 300, 2);
 
 INSERT INTO equipment_info VALUES('OUTDOOR', '健身设备', 6, 100);
 INSERT INTO equipment_info VALUES('OUTDOOR', '小区内照明灯', 6, 10);
@@ -13,35 +13,71 @@ INSERT INTO equipment_info VALUES('INDOOR', '楼层照明灯', 1, 10);
 INSERT INTO equipment_info VALUES('INDOOR', '水闸', 1, 10);
 INSERT INTO equipment_info VALUES('INDOOR', '电闸', 1, 20);
 
+INSERT INTO `resident`(resident_name, phonenumber) VALUES ('张健', '18717723912');
 
-INSERT INTO parking_space VALUES('pks1', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks2', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks3', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks4', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks5', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks6', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks7', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks8', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks9', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks10', 'c1', 'IDLE', 'PURCHASED');
-INSERT INTO parking_space VALUES('pks11', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks12', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks13', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks14', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks15', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks16', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks17', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks18', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks19', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks20', 'c1', 'IDLE', 'RENTED');
-INSERT INTO parking_space VALUES('pks21', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks22', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks23', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks24', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks25', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks26', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks27', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks28', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks29', 'c1', 'IDLE', 'TEMPORARY');
-INSERT INTO parking_space VALUES('pks30', 'c1', 'IDLE', 'TEMPORARY');
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 1, 60.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 10, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (7, 4, 1, 60.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (7, 4, 2, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (7, 4, 3, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 2, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 3, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 4, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 5, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 6, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 7, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 8, 50.00);
+INSERT INTO `residence`(floor_num, unit_num, room_num, area) VALUES (6, 4, 9, 50.00);
+
+
+INSERT INTO property_record VALUES (1, 9, '2019-01-03 15:18:36', 2000000.00);
+
+INSERT INTO `residence_record` VALUES (1, 1, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 10, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 11, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 12, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 13, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 2, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 3, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 4, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 5, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 6, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 7, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 8, 'IDLE');
+INSERT INTO `residence_record` VALUES (1, 9, 'BUSY');
+
+
+INSERT INTO parking_space VALUES(1, 1, 'BUSY', 'PURCHASED');
+INSERT INTO parking_space VALUES(2, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(3, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(4, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(5, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(6, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(7, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(8, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(9, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(10, 1, 'IDLE', 'PURCHASED');
+INSERT INTO parking_space VALUES(11, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(12, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(13, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(14, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(15, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(16, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(17, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(18, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(19, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(20, 1, 'IDLE', 'RENTED');
+INSERT INTO parking_space VALUES(21, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(22, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(23, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(24, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(25, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(26, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(27, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(28, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(29, 1, 'IDLE', 'TEMPORARY');
+INSERT INTO parking_space VALUES(30, 1, 'IDLE', 'TEMPORARY');
+
+INSERT INTO purchased_pks VALUES (1, 1, 50.00);
+
 
